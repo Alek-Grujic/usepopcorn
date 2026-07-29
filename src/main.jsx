9df5e1779c17bba4b67d125dda436b9943660db6
Alek-Grujic <alek.grujic001@outlook.com>
2026-07-29 18:03:01 +0200

    feat: save and restore watched movies from localStorage

--- src/main.jsx
+++ src/main.jsx
@@ -19,11 +19,11 @@ import Converter from "./converter.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <StarRating maxRating={10} />
     <StarRating size={32} color="red" />
     <Test /> */}
     {/* <ShowMore /> */}
-    <Converter />
+    {/* <Converter /> */}
   </StrictMode>,
 );
